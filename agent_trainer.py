9f3b080e2ce09953a9ccdbfcbcc337a5b4607175
jrfalou <jrfalou@users.noreply.github.com>
2019-12-15 15:10:06 +0000

    navigation: added AgentTrainer class that can either train or test a model

---
+++ agent_trainer.py
@@ -0,0 +1,155 @@
+import torch
+import numpy as np
+from collections import deque
+
+#internal imports
+from dqn_agent import Agent
+
+class AgentTrainer:
+    # AgentTrainer:
+    # ======
+    # env (UnityEnvironment): unity environment to interact with
+    # params (dictionary): format can be found in the ./default_params.json file
+    # results_path (string): file path to output training results
+    # debug_mode (bool): enables debug mode logging
+    def __init__(self, env, params, results_path='', debug_mode=False):
+        #Environments contain brains which are responsible for deciding the actions of their 
+        # associated agents. Here we check for the first brain available, and set it as the default 
+        # brain we will be controlling from Python.
+        self.env = env
+        self.trainer_id = params['id']
+        self.trainer_params = params['training']
+        self.agent_params = params['agent']
+        self.brain_name = self.env.brain_names[0]
+        self.results_path = results_path
+        self.debug_mode = debug_mode
+        
+        # reset the environment and print details
+        env_info = self.env.reset(train_mode=True)[self.brain_name]
+        state = env_info.vector_observations[0]
+        state_size = len(state)
+        action_size = self.env.brains[self.brain_name].vector_action_space_size
+        
+        if self.debug_mode:
+            print('Number of agents:', len(env_info.agents))
+            print('Number of actions:', action_size)
+            print('States look like:', state)
+            print('obs', env_info.vector_observations)
+            print('States have length:', state_size)
+
+        #create agent
+        self.agent = Agent(
+            state_size=state_size, 
+            action_size=action_size,
+            layers_params=self.agent_params['nn_layers'],
+            double_dqn=self.agent_params['double_dqn'],
+            step_reward=self.agent_params['step_reward'],
+            soft_tau=self.agent_params['soft_tau'],
+            learning_rate=self.agent_params['learning_rate'],
+            memory_prio_params=(
+                self.agent_params['memory_prio_params']['memory_prio_enabled'],
+                self.agent_params['memory_prio_params']['memory_prio_a'],
+                self.agent_params['memory_prio_params']['memory_prio_b0'],
+                self.agent_params['memory_prio_params']['memory_prio_b_step']
+            ),
+            debug_mode=self.agent_params['debug_mode'])
+
+    def train(self):
+        print('Train model_id', self.trainer_id)
+        scores = []
+        scores_window = deque(maxlen=100)
+        eps = self.trainer_params['eps_start']
+        consecutive_worse_count = 0
+        total_worse_count = 0
+        for i_episode in range(1, self.trainer_params['n_episodes']+1):
+            # reset the environment and get current state
+            env_info = self.env.reset(train_mode=True)[self.brain_name]
+            state = env_info.vector_observations[0]
+            
+            #train agent with another episode
+            score = 0
+            for t in range(self.trainer_params['max_t']):
+                # total_step_t = datetime.datetime.now()
+                action = self.agent.act(state, eps).astype(int)
+                
+                # env_step_t = datetime.datetime.now()
+                env_info = self.env.step(action)[self.brain_name]
+                next_state = env_info.vector_observations[0]
+                reward = env_info.rewards[0]
+                done = env_info.local_done[0]
+                # agent.time_analysis.env_step.append(datetime.datetime.now()-env_step_t)
+                
+                self.agent.step(state, action, reward, next_state, done)
+                state = next_state
+                score += reward
+                # agent.time_analysis.total_step.append(datetime.datetime.now()-total_step_t)
+                if done:
+                    break 
+
+            #post-processing of latest episode
+            scores_window.append(score)
+            scores.append(score)
+            eps = max(self.trainer_params['eps_end'], self.trainer_params['eps_decay']*eps)
+            
+            print('\rEpisode {}\tAverage Score: {:.2f}\teps: {:.5f}'.format(i_episode, np.mean(scores_window), eps), end="")
+
+            #post-processing of latest 100 episodes        
+            if i_episode % scores_window.maxlen == 0:
+                # agent.time_analysis.to_str()
+                # agent.time_analysis.reset()
+                
+                if i_episode > scores_window.maxlen:
+                    if np.mean(scores_window) < prev_scores_window_avg:
+                        consecutive_worse_count += 1
+                        total_worse_count += 1
+                    else:
+                        consecutive_worse_count = 0
+
+                prev_scores_window_avg = np.mean(scores_window)
+                print('\rEpisode {}\tAverage Score: {:.2f}\teps: {:.5f}'.format(i_episode, np.mean(scores_window), eps))
+                self.write_score_to_file(i_episode=i_episode, mean_score=np.mean(scores_window))
+
+            #test if agent solved the environment or failed
+            #if the environment was solved, save the agent model weights
+            if np.mean(scores_window) >= self.trainer_params['max_score']:
+                print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+                torch.save(self.agent.qnetwork_local.state_dict(), self.agent_params['model_tag'] + '.pth')
+                self.write_score_to_file(i_episode=i_episode, mean_score=np.mean(scores_window))
+                break
+            elif total_worse_count == self.trainer_params['max_total_worse'] or consecutive_worse_count == self.trainer_params['max_consec_worse']:
+                print('\nEnvironment failed in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+                break
+
+    def write_score_to_file(self, i_episode, mean_score):
+        results_file = open(self.results_path, 'a')
+        results_file.write(','.join([str(self.trainer_id), self.agent_params['model_tag'].replace(',', ';'), str(i_episode), str(mean_score)]) + '\n')
+        results_file.close()
+                
+    def test(self, model_weights=''):
+        brain = self.env.brains[self.brain_name]
+
+        # reset the environment and get current state
+        env_info = self.env.reset(train_mode=False)[self.brain_name]
+        state = env_info.vector_observations[0]
+        action_size = brain.vector_action_space_size
+        score = 0
+        
+        if model_weights != '':
+            self.agent.qnetwork_local.load_state_dict(torch.load(model_weights + '.pth'))
+
+        while True:
+            if model_weights=='random':
+                action = self.agent.act(state).astype(int)
+            else:
+                action = np.random.randint(action_size)
+
+            env_info = self.env.step(action)[self.brain_name]
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            score += reward
+            state = next_state
+            if done:
+                break
+                
+            print("\rScore: {}".format(score), end='')
--- navigation.py
+++ navigation.py
@@ -1,177 +1,14 @@
 #external imports
 from unityagents import UnityEnvironment
-from collections import deque
 import numpy as np
-import matplotlib.pyplot as plt
-import torch
+# import matplotlib.pyplot as plt
 import argparse
 import json
-import sys
-import time
 
-#internal imports
-from dqn_agent import Agent
-
-def train_agent(env, params, results_path=''):
-    #Params
-    #======
-    # env (UnityEnvironment): unity environment to interact with
-    # params (dictionary): format can be found in the ./default_params.json file
-    # results_path (string): file path to output training results
-
-    #Environments contain brains which are responsible for deciding the actions of their 
-    # associated agents. Here we check for the first brain available, and set it as the default 
-    # brain we will be controlling from Python.
-    brain_name = env.brain_names[0]
-    brain = env.brains[brain_name]
-
-    # reset the environment and print details
-    env_info = env.reset(train_mode=True)[brain_name]
-    action_size = brain.vector_action_space_size
-    state = env_info.vector_observations[0]
-    state_size = len(state)
+#local imports
+from agent_trainer import AgentTrainer
     
-    print('Number of agents:', len(env_info.agents))
-    print('Number of actions:', action_size)
-    print('States look like:', state)
-    print('obs', env_info.vector_observations)
-    print('States have length:', state_size)
-
-    #create agent
-    agent = Agent(
-        state_size=state_size, 
-        action_size=action_size,
-        layers_params=params['agent']['nn_layers'],
-        double_dqn=params['agent']['double_dqn'],
-        step_reward=params['agent']['step_reward'],
-        soft_tau=params['agent']['soft_tau'],
-        learning_rate=params['agent']['learning_rate'],
-        memory_prio_params=(
-            params['agent']['memory_prio_params']['memory_prio_enabled'],
-            params['agent']['memory_prio_params']['memory_prio_a'],
-            params['agent']['memory_prio_params']['memory_prio_b0'],
-            params['agent']['memory_prio_params']['memory_prio_b_step']
-        ),
-        debug_mode=params['agent']['debug_mode'])
-
-    #train agent while interacting with environment
-    scores = []
-    scores_window = deque(maxlen=100)
-    eps = params['training']['eps_start']
-    consecutive_worse_count = 0
-    total_worse_count = 0
-    for i_episode in range(1, params['training']['n_episodes']+1):
-        
-        # reset the environment and get current state
-        env_info = env.reset(train_mode=True)[brain_name]
-        state = env_info.vector_observations[0]
-        
-        #train agent with another episode
-        score = 0
-        for t in range(params['training']['max_t']):
-            # total_step_t = datetime.datetime.now()
-            action = agent.act(state, eps).astype(int)
-            
-            # env_step_t = datetime.datetime.now()
-            env_info = env.step(action)[brain_name]
-            next_state = env_info.vector_observations[0]
-            reward = env_info.rewards[0]
-            done = env_info.local_done[0]
-            # agent.time_analysis.env_step.append(datetime.datetime.now()-env_step_t)
-            
-            agent.step(state, action, reward, next_state, done)
-            state = next_state
-            score += reward
-            # agent.time_analysis.total_step.append(datetime.datetime.now()-total_step_t)
-            if done:
-                break 
-
-        #post-processing of latest episode
-        scores_window.append(score)
-        scores.append(score)
-        eps = max(params['training']['eps_end'], params['training']['eps_decay']*eps)
-        
-        print('\rEpisode {}\tAverage Score: {:.2f}\teps: {:.5f}'.format(i_episode, np.mean(scores_window), eps), end="")
-
-        #post-processing of latest 100 episodes        
-        if i_episode % scores_window.maxlen == 0:
-            # agent.time_analysis.to_str()
-            # agent.time_analysis.reset()
-            
-            if i_episode > scores_window.maxlen:
-                if np.mean(scores_window) < prev_scores_window_avg:
-                    consecutive_worse_count += 1
-                    total_worse_count += 1
-                else:
-                    consecutive_worse_count = 0
-
-            prev_scores_window_avg = np.mean(scores_window)
-            print('\rEpisode {}\tAverage Score: {:.2f}\teps: {:.5f}'.format(i_episode, np.mean(scores_window), eps))
-            results_file = open(results_path, 'a')
-            results_file.write(','.join([str(params['id']), params['agent']['model_tag'].replace(',', ';'), str(i_episode), str(np.mean(scores_window))]) + '\n')
-            results_file.close()
-
-        #test if agent solved the environment or failed
-        #if the environment was solved, save the agent model weights
-        if np.mean(scores_window)>=params['training']['max_score']:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-            torch.save(agent.qnetwork_local.state_dict(), params['agent']['model_tag'] + '.pth')
-            results_file = open(results_path, 'a')
-            results_file.write(','.join([str(params['id']), params['agent']['model_tag'].replace(',', ';'), str(i_episode), str(np.mean(scores_window))]) + '\n')
-            results_file.close()
-            break
-        elif total_worse_count == params['training']['max_total_worse'] or consecutive_worse_count == params['training']['max_consec_worse']:
-            print('\nEnvironment failed in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-            break
-
-def test_agent(env, params, model='random'):
-    brain_name = env.brain_names[0]
-    brain = env.brains[brain_name]
-
-    # reset the environment and get current state
-    env_info = env.reset(train_mode=False)[brain_name]
-    state = env_info.vector_observations[0]
-    action_size = brain.vector_action_space_size
-    state_size = len(state)
-    score = 0
-    
-    agent = None
-    if model != 'random':
-        agent = Agent(
-            state_size=state_size, 
-            action_size=action_size,
-            layers_params=params['agent']['nn_layers'],
-            double_dqn=params['agent']['double_dqn'],
-            step_reward=params['agent']['step_reward'],
-            soft_tau=params['agent']['soft_tau'],
-            learning_rate=params['agent']['learning_rate'],
-            memory_prio_params=(
-                params['agent']['memory_prio_params']['memory_prio_enabled'],
-                params['agent']['memory_prio_params']['memory_prio_a'],
-                params['agent']['memory_prio_params']['memory_prio_b0'],
-                params['agent']['memory_prio_params']['memory_prio_b_step']
-            ),
-            debug_mode=params['agent']['debug_mode'])
-        agent.qnetwork_local.load_state_dict(torch.load(model + '.pth'))
-
-    while True:
-        if agent:
-            action = agent.act(state).astype(int)
-        else:
-            action = np.random.randint(action_size)
-
-        env_info = env.step(action)[brain_name]
-        next_state = env_info.vector_observations[0]
-        reward = env_info.rewards[0]
-        done = env_info.local_done[0]
-        score += reward
-        state = next_state
-        if done:
-            break
-            
-        print("\rScore: {}".format(score), end='')
-    
-def plot_scores(scores):
+# def plot_scores(scores):
     # import torch
     # import numpy as np
     # import matplotlib.pyplot as plt
@@ -181,12 +18,12 @@ def plot_scores(scores):
     #     from IPython import display
     # plt.ion()
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    plt.plot(np.arange(len(scores)), scores)
-    plt.ylabel('Score')
-    plt.xlabel('Episode #')
-    plt.show()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # plt.plot(np.arange(len(scores)), scores)
+    # plt.ylabel('Score')
+    # plt.xlabel('Episode #')
+    # plt.show()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parameters for navigation project')
@@ -198,16 +35,18 @@ if __name__ == "__main__":
     parser.add_argument('--train_results_path', dest='train_results_path', default='./results.csv')
     args = parser.parse_args()
     
-    if args.mode == 'test':
-        env = UnityEnvironment(file_name="./Banana_Windows_x86_64/Banana.exe")
-        test_agent(env, params=args.test_params[0], model=args.test_model)
-        env.close()
-    elif args.mode == 'train':
-        params_file = open(args.train_params, 'r')
-        training_params = json.loads(params_file.read())
-        params_file.close()
+    env = None
+    try:
+        if args.mode == 'test':
+            env = UnityEnvironment(file_name="./Banana_Windows_x86_64/Banana.exe")
+            agent_trainer = AgentTrainer(env=env, params=args.test_params[0])
+            agent_trainer.test(model_weights=args.test_model)
+            env.close()
+        elif args.mode == 'train':
+            params_file = open(args.train_params, 'r')
+            training_params = json.loads(params_file.read())
+            params_file.close()
 
-        try:
             env = UnityEnvironment(
                 file_name="./Banana_Windows_x86_64/Banana.exe",
                 no_graphics=True
@@ -215,14 +54,20 @@ if __name__ == "__main__":
             for training_params in training_params['training_params']:
                 if(training_params['id'] < int(args.train_start_id)):
                     continue
-                print('Train model_id', training_params['id'])
-                
-                train_agent(env, training_params, args.train_results_path)
-        except Exception as e:
-            print('Unexpected error:', str(e))
-            raise(e)
-        finally:
-            env.close()
+
+                agent_trainer = AgentTrainer(
+                    env=env, 
+                    params=training_params, 
+                    results_path=args.train_results_path
+                )
+                agent_trainer.train()
+
             print('Training ended')
-    else:
-        print('Unknown mode', args.mode)
+        else:
+            print('Unknown mode', args.mode)
+    except Exception as e:
+        print('Unexpected error:', str(e))
+        raise(e)
+    finally:
+        if env is not None:
+            env.close()
